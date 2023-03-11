2b476e2aa6b8be11a1d68e2c5e52a18559b82070
chang_geng <2154695037@qq.com>
2023-03-11 19:32:34 +0800

    重构FL训练过程，2客户机条件下单元测试完毕，等待整合

--- client.py
+++ client.py
@@ -1,61 +1,35 @@
-import numpy as np
+import os
 import keras
-import time
 import pickle
 import codecs
-from keras import Input, Model, Sequential
-# from keras.datasets import mnist
-from keras.layers import Dense, LeakyReLU, BatchNormalization, Reshape
+from keras.datasets import mnist
 from keras.models import model_from_json
-from socketIO_client import SocketIO, LoggingNamespace
-from BFV import encrypt
+import socketio
 from BFV import bfv
 import key_generation
 from SM9.gmssl import sm9
-
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 class LocalModel(object):
-    def __init__(self, model_config):
-        self.training_start_time = int(round(time.time()))
-        self.model_config = model_config
-        self.model = model_from_json(model_config['model_json'])
+    def __init__(self,model_id,model_config):
+        self.model_id=model_id
+        self.model = model_from_json(model_config)
         # self.model.summary()
-        self.discriminator = self.model
-        # self.discriminator.summary()
-        # optimizer = keras.optimizers.SGD(lr=0.1, momentum=0.9, decay=0.0, nesterov=False)
-        # TODO:改为传统mnist
-        self.latent_dim = 100
-        self.img_rows = 28
-        self.img_cols = 28
-        self.channels = 3
-        self.img_shape = (self.img_rows, self.img_cols, self.channels)
-        self.discriminator.compile(optimizer=keras.optimizers.Adam(lr=0.005),
+        self.discriminator=self.model
+        self.discriminator.compile(optimizer=keras.optimizers.Adam(learning_rate=0.005),
                                    loss='sparse_categorical_crossentropy',
                                    metrics=['accuracy'])
 
-    def train(self, batch_size, req_id):
-        # TODO:改变训练集后需要检查
-        # (X_train, y_train), (_, _) = mnist.load_data()
-        pne_data = np.load('./DataSet/bloodmnist.npz')
-        print(pne_data.files)
-        x_train = pne_data['train_images']
-        y_train = pne_data['train_labels']
-        x_test = pne_data['test_images']
-        y_test = pne_data['test_labels']
-        x_val = pne_data['val_images']
-        y_val = pne_data['val_labels']
-        print('Train: ', x_train.shape, y_train.shape)
-        print('Test: ', x_test.shape, y_test.shape)
-        print('Val: ', x_val.shape, y_val.shape)
+    def train(self):
+        '''
+        训练一次
+        '''
+        print('开始训练,当前模型id为: ',self.model_id)
+        (x_train, y_train),(x_test, y_test) = mnist.load_data()
+        # print('Train: ', x_train.shape, y_train.shape)
+        # print('Test: ', x_test.shape, y_test.shape)
         x_train = x_train / 255.0
         x_test = x_test / 255.0
-        x_val = x_val / 255.0
-        if req_id == 1:
-            x_train = x_train[:6000]
-            y_train = y_train[:6000]
-        else:
-            x_train = x_train[6000:]
-            y_train = y_train[6000:]
         # for j in range(8):
         #     for i in range(len(y_val)):
         #         if y_val[i] == j:
@@ -66,214 +40,152 @@ class LocalModel(object):
         # print(w1.shape)
         self.discriminator.fit(x_train, y_train, epochs=1, batch_size=64, shuffle=True)
         test = self.discriminator.evaluate(x_test, y_test, verbose=0)
-        f = open("result.txt", "a")
-        print(f"test_loss:{test[0]}, test_acc:{test[1]}", file=f)
-        f.close()
-
         return self.discriminator.get_weights(), test[0], test[1]
 
     def get_weights(self):
         return self.model.get_weights()
 
-    def set_weights2(self, new_weights):
+    def set_weights(self,new_weights):
         self.discriminator.set_weights(new_weights)
 
-
 class FederatedClient(object):
-    MAX_DATASET_SIZE_KEPT = 1200  # 训练的最大数据集大小？
+    def __init__(self,server_host, server_port):
+        # 训练所用本地模型及轮数
+        self.local_model=None
+        self.round_num=0
 
-    def __init__(self, server_host, server_port):
-        # TODO:存在冗余信息,可改
-        self.local_model = None
-        self.req_num = 1
-        self.id = None
-        self.num = 0
-        self.t1 = 0
-        self.t2 = 0
-
-        # 生成公私钥对
-        # self.pk, self.sk = BFV.generate_keypair(128)
+        # 生成公私钥对,用于模型权重加密
         self.pk = bfv.publicKey(key_generation.N, key_generation.G)
         self.sk = bfv.privateKey(key_generation.N, key_generation.LAM, key_generation.G)
-        print("public_key.n:", self.pk.n)
-        print("public_key.g:", self.pk.g)
-        print("secret_key.lam:", self.sk.lam)
 
-        # sm9
-        self.master_public, self.master_secret = sm9.setup('sign') #生成客户端的公私钥
+        # sm9,用于连接认证
+        self.master_public, self.master_secret = sm9.setup('sign')
 
-        self.sio = SocketIO(server_host, server_port, LoggingNamespace)
-        self.register_handles()
-        print("启动客户端(sent wakeup)")
-        self.t1 = time.time()
-        self.sio.emit('client_wake_up')
-        self.sio.wait()
+        # 客户端参数
+        self.sio=socketio.Client()
+        self.sHost=server_host
+        self.sPort=server_port
+        self.sid=None
+        self.regist_connect_handles()
+        self.regist_update_handles()
 
-    def on_init(self, *args):
-        '''
-        接收服务器发送的模型,初始化本地模型,并请求更新
-        '''
-        self.t2 = time.time()
-        f = open("expense.txt", "a")
-        print(f"认证总用时:{self.t2 - self.t1}s", file=f)
-        f.close()
-        model_config = args[0]
-        print('初始化(on init)', model_config)
-        self.local_model = LocalModel(model_config)
-
-        # ready to be dispatched for training
-        print('请求更新(preparing sent client_ready)')
-        self.sio.emit('client_ready', {
-            'class_distr': 1
-        })
-
-    def on_authentication(self, *args):
-        '''
-        进行签名认证,并回发签名与公钥
-        '''
-        req = args[0]
-        self.id = req['id']
-        self.num = req['num']
-        print('开始签名...')
-        t1 = time.time()
-        # sm9签名认证
-        # if self.num == 1:
-        # master_public, master_secret = key_generation.P_A, key_generation.S_A
-        print(f"master_public:{self.master_public}")
-        print(f"master_secret:{self.master_secret}")
-        ida = f"{self.id}"
-        print(f"id:{ida}")
-        Da = sm9.private_key_extract('sign', self.master_public, self.master_secret, ida)
-        print(f"DA:{Da}")
-        message = 'connect'
-        print(f"message:{message}")
-        signature = sm9.sign(self.master_public, Da, message)
-        print(f"signature:{signature}")
-        # elif self.num == 2:
-        #     # master_public, master_secret = key_generation.P_B, key_generation.S_B
-        #     print(f"master_public:{master_public}")
-        #     print(f"master_secret:{master_secret}")
-        #     idb = f"{self.id}"
-        #     Da = sm9.private_key_extract('sign', master_public, master_secret, idb)
-        #     print(f"DA:{Da}")
-        #     message = 'connect'
-        #     print(f"message:{message}")
-        #     signature = sm9.sign(master_public, Da, message)
-        #     print(f"signature:{signature}")
-        print("签名完成...")
-        t2 = time.time()
-        f = open("expense.txt", "a")
-        print(f"用户签名计算用时:{t2 - t1}s", file=f)
-        f.close()
-        self.sio.emit('signature', {
-            'signature': obj_to_pickle_string(signature),
-            'master_public': obj_to_pickle_string(self.master_public),
-        })
-
-    def register_handles(self):
+    def regist_connect_handles(self):
         '''
         注册客户机处理句柄
         '''
-        def req_train(*args):
+        @self.sio.on('connect')
+        def on_connect():   # debug用
+            print('connect')
+
+        @self.sio.on('disconnect')
+        def on_disconnect():    # debug用
+            print('disconnect')
+
+        @self.sio.on('reconnect')
+        def on_reconnect(): # debug用
+            print('reconnect')
+
+        @self.sio.on('authentication')
+        def on_authentication(data):
+            '''
+            进行签名认证,并回发签名与公钥
+            '''
+            self.sid=data['id']
+            print('连接完成,开始签名...')
+            # sm9签名认证
+            # print(f"master_public:{self.master_public}")
+            # print(f"master_secret:{self.master_secret}")
+            ida = f"{self.sid}"
+            # print(f"id:{ida}")
+            Da = sm9.private_key_extract('sign', self.master_public, self.master_secret, ida)
+            # print(f"DA:{Da}")
+            message = 'connect'
+            # print(f"message:{message}")
+            signature = sm9.sign(self.master_public, Da, message)
+            # print(f"signature:{signature}")
+            print("签名完成,发送签名进行认证...")
+            self.sio.emit('signature', {
+                'signature': self.obj_to_pickle_string(signature),
+                'master_public': self.obj_to_pickle_string(self.master_public),
+            })
+
+        @self.sio.on('success')
+        def on_success():
+            '''
+            接收服务器的成功消息,并回发模型拉取请求
+            '''
+            print("服务器端签名认证成功")
+            self.sio.emit('client_ready')
+
+        @self.sio.on('init')
+        def on_init(data):
+            '''
+            接收服务器发送的模型,初始化本地模型,并请求更新
+            '''
+            print('初始化模型: ', data['model_id'])
+            self.local_model = LocalModel(data['model_id'],data['model_json'])
+
+    def regist_update_handles(self):
+
+        @self.sio.on('request_update')
+        def on_request_update(data):
+            '''
+            接收服务器传参,并对本地的模型进行一轮训练,回发本地模型权值
+            '''
+            self.round_num=data['round_number']
+            weights=self.pickle_string_to_obj(data['current_weights'])
+
+            # 初始训练参数不需要解密
+            # if data['round_number'] != 1:
+            #     print("解密中...")
+            #     weights = encrypt.decryption(self.sk, weights)
+            #     print("解密完成,开始训练...")
+            #     print(f"weights:{weights}")
+
+            self.local_model.set_weights(weights)
+            my_weights,_,_ = self.local_model.train()
+
+            # print(f"weights:{my_weights}")
+            # # 给参数加密
+            # print("正在加密...")
+            # my_weights = encrypt.encryption(self.pk, my_weights)
+            # print("加密完成，上传参数中...")
+            self.sio.emit('client_update',{
+                'round_number': self.round_num,
+                'weights': self.obj_to_pickle_string(my_weights),
+            })
+            print("上传参数完成")
+
+        @self.sio.on('update_ack')
+        def req_train():
             '''
             接收服务器ACK,重新回到ready状态等待服务器下一次更新请求
             '''
             # time.sleep(2)
             # print('当前时间：',time.strftime('%H-%M-%S',time.localtime(time.time())))
-            self.req_num += 1
-            print('请求第' + str(self.req_num) + '次更新：')
-            self.sio.emit('client_ready', {'req_num': self.req_num})
+            print(f'第{self.round_num}次更新完成')
 
-        def on_connect():   # debug用
-            print('connect')
+    def start(self):
+        self.sio.connect(f'http://{self.sHost}:{self.sPort}')
+        print("启动客户端并自动连接中...")
+        self.sio.emit('client_wake_up') # 自动连接
+        self.sio.wait()
 
-        def on_disconnect():    # debug用
-            print('disconnect')
+    def obj_to_pickle_string(self,x):
+        '''
+        python对象转为2进制并序列化为base64
+        '''
+        return codecs.encode(pickle.dumps(x), "base64").decode()
 
-        def on_reconnect(): # debug用
-            print('reconnect')
-
-        def on_success(*args):
-            '''
-            接收服务器的成功消息,并回发模型拉取请求
-            '''
-            print("签名认证成功...")
-            self.sio.emit('client_connect')
-
-        def on_request_update(*args):
-            '''
-            接收服务器传参,并对本地的模型进行一轮训练,回发本地模型权值
-            '''
-            req = args[0]
-            round_number1 = str(req['round_number'])
-            print('总轮数：' + round_number1)
-
-            if req['weights_format'] == 'pickle':
-                weights = pickle_string_to_obj(req['current_weights'])
-
-            # 初始训练参数不需要解密
-            if req['round_number'] != 1:
-                print("解密中...")
-                t3 = time.time()
-                weights = encrypt.decryption(self.sk, weights)
-                t4 = time.time()
-                f = open("expense.txt", "a")
-                print(f"解密用时:{t4 - t3}s", file=f)
-                f.close()
-                print("解密完成,开始训练...")
-                print(f"weights:{weights}")
-
-            self.local_model.set_weights2(weights)
-
-            my_weights, train_loss, train_accuracy = self.local_model.train(batch_size=128, req_id=self.id)
-
-            # f1 = open("weights.txt", "w")
-            # print(f"weights:{my_weights}", file=f1)
-
-            print(f"weights:{my_weights}")
-            # 给参数加密
-            t1 = time.time()
-            print("正在加密...")
-            my_weights = encrypt.encryption(self.pk, my_weights)
-
-            # f2 = open("en_weights.txt", "w")
-            # print(f"en_weights:{my_weights}", file=f2)
-
-            t2 = time.time()
-            f = open("expense.txt", "a")
-            print(f"加密用时:{t2 - t1}s", file=f)
-            f.close()
-            resp = {
-                'round_number': req['round_number'],
-                'weights': obj_to_pickle_string(my_weights),
-            }
-            print("加密完成，上传参数中...")
-            self.sio.emit('client_update', resp)
-        # TODO:绑定风格不同,可改
-        self.sio.on('next', req_train)
-        self.sio.on('connect', on_connect)
-        self.sio.on('disconnect', on_disconnect)
-        self.sio.on('reconnect', on_reconnect)
-        self.sio.on('init', lambda *args: self.on_init(*args))
-        self.sio.on('request_update', on_request_update)
-        self.sio.on('authentication', lambda *args: self.on_authentication(*args))
-        self.sio.on('success', on_success)
-
-
-def obj_to_pickle_string(x):
-    '''
-    python对象转为2进制并序列化为base64
-    '''
-    return codecs.encode(pickle.dumps(x), "base64").decode()
-
-
-def pickle_string_to_obj(s):
-    '''
-    从base64反序列化python对象
-    '''
-    return pickle.loads(codecs.decode(s.encode(), "base64"))  # 模型返序列化loads，编解码en/decode
+    def pickle_string_to_obj(self,s):
+        '''
+        从base64反序列化python对象
+        '''
+        return pickle.loads(codecs.decode(s.encode(), "base64"))  # 模型返序列化loads，编解码en/decode
 
 
 if __name__ == "__main__":
     # TODO:准备改为嵌入式
     client = FederatedClient("127.0.0.1", 5000)
+    client.start()
+    pass
--- server.py
+++ server.py
@@ -1,23 +1,19 @@
 # -*- coding:utf-8 -*-
 import pickle
-import time
-
 import keras
 import uuid
 from keras.models import Sequential
 import codecs
-import numpy as np
 import json
 from keras.layers import Dense, Dropout, Flatten, Activation, Convolution2D, MaxPooling2D
 from flask import *
 from flask_socketio import *
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 # turn off url logging
 import logging
-
-from BFV import bfv
 from SM9.gmssl import sm9
-import key_generation
+
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
@@ -29,37 +25,36 @@ class GlobalModel(object):  #虚父类
     def __init__(self):
         self.model = self.build_model()
         self.current_weights = self.model.get_weights()
-        # TODO:无用
-        self.img_shape = (28, 28, 3)    # blood mnist单输入大小
 
     def build_model(self):
-        # raise NotImplementedError()
         pass
 
     def update_weights(self, client_weights):
-        new_weights = [np.zeros(w.shape) for w in self.current_weights]
-        # FIXME:此处权值更新操作有误
-        for i in range(len(new_weights)):
-            new_weights[i] = client_weights[i]
-        self.current_weights = new_weights
-        # FIXME:此处权值更新完毕后未加载至当前模型
-        print('服务器更新成功！')
+        '''
+        更新权值(未加载),由于不知道客户端数目,故不在此做平均
+        '''
+        for i in range(len(client_weights)):
+            self.current_weights[i]+=client_weights[i]
+    
+    def update_weights2(self,clientNum):
+        for i in range(len(self.current_weights)):
+            self.current_weights[i]=self.current_weights[i]/clientNum
+        self.model.set_weights(self.current_weights)
 
 
 class GlobalModel_MNIST_CNN(GlobalModel):
     '''
     此处使用的数据集为blood mnist(28*28*3大小,8类别)
     '''
-    # TODO:改为传统mnist
     def __init__(self):
         super(GlobalModel_MNIST_CNN, self).__init__()
 
     def build_model(self):
         '''
-        28*28*3输入,8概率输出
+        28*28*1输入,10概率输出
         '''
         model = Sequential()
-        model.add(Convolution2D(32, (3, 3), input_shape=(28, 28, 3)))
+        model.add(Convolution2D(32, (3, 3), input_shape=(28, 28, 1)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2),data_format="channels_first"))
         model.add(Convolution2D(32, (3, 3)))
@@ -68,41 +63,38 @@ class GlobalModel_MNIST_CNN(GlobalModel):
         model.add(Convolution2D(64, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2),data_format="channels_first"))
-        model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+        model.add(Flatten())
         model.add(Dense(64))
         model.add(Activation('relu'))
         model.add(Dropout(0.3))
-        model.add(Dense(8))
+        model.add(Dense(10))
         model.add(Activation('softmax'))
-        model.summary()
+        # model.summary()
 
-        model.compile(optimizer=keras.optimizers.Adam(lr=0.005),
-                      loss='sparse_categorical_crossentropy',
-                      metrics=['accuracy'])
+        model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.005),
+                        loss='sparse_categorical_crossentropy',
+                        metrics=['accuracy'])
         return model
 
 
 class FLServer(object):
     def __init__(self, global_model, host, port):
-        self.global_model = global_model()
+        self.global_model=global_model()
 
-        self.ready_client_sids = set()  # 已连接的客户端sid集合
+        self.ready_client_sids=set()    # 已连接并就绪的客户端sid
+        self.block_client_sids=set()    # 等待回发更新的客户端sid
+        self.block_client_num=0
 
-        # TODO:演示所用,将客户端更新范围限制于first_id和second_id所指定的2个客户端,flag用于管理更新切换,可改
-        self.flag = 1
-        self.first_id = 0   
-        self.second_id = 0
+        self.app=Flask(__name__)
+        self.socketio=SocketIO(self.app)
+        self.host=host
+        self.port=port
+        self.model_id=str(uuid.uuid4()) # 随机生成uuid
+        self.current_round=0            # 全局模型训练轮数
+        self.regist_connect_handles()
+        self.regist_update_handles()
 
-        self.app = Flask(__name__)
-        self.socketio = SocketIO(self.app)
-        self.host = host
-        self.port = port
-        self.model_id = str(uuid.uuid4())  # 随机生成uuid
-        self.num = 0    # 已连接的客户端数量
-        self.current_round = 0  # 全局模型训练轮数
-        self.current_round_client_updates = []  # 当前轮数的到的客户端更新数据,包含轮数及base64编码的权重信息
-        self.client_updates_weights = []    # 当前轮数得到的解码后的更新权重
-        self.register_handles()
+        print(f'当前全局模型uuid: {self.model_id}')
 
         @self.app.route('/')
         def dashboard():    # 无用
@@ -112,29 +104,28 @@ class FLServer(object):
         def status_page():
             return json.dumps(self.global_model.get_stats())
 
-    def register_handles(self):
+    def regist_connect_handles(self):
 
         @self.socketio.on('connect')
         def handle_connect():   # debug用
             '''
             客户端连接
             '''
-            pass
-            # print(request.sid[0], "connected")
+            print(f"用户 {request.sid} 连接")
 
         @self.socketio.on('reconnect')
         def handle_reconnect(): # debug用
             '''
             客户端重连
             '''
-            print(request.sid, "reconnected")
+            print(f"用户 {request.sid} 重连")
 
         @self.socketio.on('disconnect')
         def handle_reconnect():
             '''
             客户端断连,将客户端对应的sid从连接集合中去除
             '''
-            print(request.sid, "disconnected")
+            print(f"用户 {request.sid} 断开连接")
             if request.sid in self.ready_client_sids:
                 self.ready_client_sids.remove(request.sid)
 
@@ -143,11 +134,9 @@ class FLServer(object):
             '''
             客户端启动,回送认证请求
             '''
-            self.num += 1
-            print("开始身份认证：", request.sid)
+            print(f"开始用户 {request.sid} 身份认证")
             emit('authentication', {
-                'id': request.sid,
-                'num': self.num,
+                'id': request.sid
             })
 
         @self.socketio.on('signature')
@@ -155,110 +144,85 @@ class FLServer(object):
             '''
             对客户机回发的签名和公钥进行验证,并回发成功消息
             '''
-            print("开始验证签名...")
-            t1 = time.time()
-            # if self.num == 1:
-            # master_public = key_generation.P_A
-            # req = data[0]
-            master_public = pickle_string_to_obj(data['master_public'])
+            print(f"开始验证用户 {request.sid} 签名...")
+            master_public = self.pickle_string_to_obj(data['master_public'])
             idA = f"{request.sid}"
             message = "connect"
-            signature = pickle_string_to_obj(data['signature'])
-            print(f"master_public:{master_public}")
-            print(f"message:{message}")
-            print(f"id:{idA}")
-            print(f"signature:{signature}")
-            assert (sm9.verify(master_public, idA, message, signature))
+            signature = self.pickle_string_to_obj(data['signature'])
+            # print(f"master_public:{master_public}")
+            # print(f"message:{message}")
+            # print(f"id:{idA}")
+            # print(f"signature:{signature}")
+            assert (sm9.verify(master_public, idA, message, signature)) #断言验证
             print("认证成功...")
-            t2 = time.time()
-            f = open("expense.txt", "a")
-            print(f"服务器验证签名用时:{t2 - t1}s", file=f)
-            f.close()
-            # elif self.num == 2:
-            #     master_public = key_generation.P_B
-            #     idB = f"{request.sid}"
-            #     message = "connect"
-            #     signature = data
-            #     assert (sm9.verify(master_public, idB, message, signature))
             emit('success')
 
-        @self.socketio.on('client_connect')
-        def handle_wake_up():
+        @self.socketio.on('client_ready')
+        def handle_client_ready():
             '''
             接收客户机请求,回发json格式模型与当前模型id
             '''
-            print("认证成功,开始连接客户: ", request.sid)
+            print(f'开始初始化用户 {request.sid} 模型')
             emit('init', {
                 'model_json': self.global_model.model.to_json(),
                 'model_id': self.model_id,
-            })
+            },callback=after_connect)
 
-        @self.socketio.on('client_ready')
-        def handle_client_ready(data):
-            '''
-            将已完成认证并初始化的客户机加入已连接集合中,此时新建连接过程完成
-            '''
-            # print("client ready for training", request.sid, data)
-            print('客户端', request.sid, '请求更新')
+        def after_connect():
+            print(f'用户 {request.sid} 本地模型初始化成功')
             self.ready_client_sids.add(request.sid)
-            if len(self.ready_client_sids) == 2:    #当正好有两个客户机连接时，初始化first_id和second_id,并进行一轮训练
-                self.first_id = request.sid
-                for idd in self.ready_client_sids:
-                    if idd != self.first_id:
-                        self.secend_id = idd
-                self.train_next_round(self.first_id)
+            if len(self.ready_client_sids)==2:  # debug用
+                self.train_next_round()
+
+    def regist_update_handles(self):
 
         @self.socketio.on('client_update')
         def handle_client_update(data):
             '''
             接收客户端权值,更新全局模型,并回发ACK
             '''
-            print("处理客户", request.sid, '的更新')
-            self.current_round_client_updates = [data]
-            self.client_updates_weights = pickle_string_to_obj(data['weights'])
-            print(self.client_updates_weights)
-            self.global_model.update_weights(self.client_updates_weights)
-            if self.flag == 1:
-                self.flag == 2
-                emit('next', room=self.secend_id)
-            else:
-                self.flag==1
-                emit('next', room=self.first_id)
+            print("处理客户", request.sid,'的更新')
+            if self.current_round==data['round_number']:
+                client_updates_weights = self.pickle_string_to_obj(data['weights'])
+                self.global_model.update_weights(client_updates_weights)
+                emit('update_ack')
+                self.ready_client_sids.add(request.sid)
+                self.block_client_sids.discard(request.sid)
+                if not self.block_client_sids:
+                    self.global_model.update_weights2(self.block_client_num)
+                    print(f'所有 {self.block_client_num} 个客户机更新完毕')
 
-    def train_next_round(self, id):
+    def train_next_round(self):
         '''
         服务器轮次计数器加1,并请求客户端的更新权值以便聚合
         '''
-        self.current_round += 1
-        self.current_round_client_updates = []
-        print("总轮次 ", self.current_round)
-        emit('request_update', {
+        self.current_round+=1
+        print("全局模型轮次: ", self.current_round)
+        emit('request_update',{
             'model_id': self.model_id,
             'round_number': self.current_round,
-            'current_weights': obj_to_pickle_string(self.global_model.current_weights),
-            'weights_format': 'pickle',
-        }, room=id)
+            'current_weights': self.obj_to_pickle_string(self.global_model.current_weights)
+        },broadcast=True)
+        self.block_client_sids=self.ready_client_sids.copy()
+        self.block_client_num=len(self.block_client_sids)
+        self.ready_client_sids.clear()
 
     def start(self):
         self.socketio.run(self.app, host=self.host, port=self.port)
 
+    def obj_to_pickle_string(self,x):
+        '''
+        python对象转为2进制并序列化为base64
+        '''
+        return codecs.encode(pickle.dumps(x), "base64").decode()
 
-def obj_to_pickle_string(x):
-    '''
-    python对象转为2进制并序列化为base64
-    '''
-    return codecs.encode(pickle.dumps(x), "base64").decode()
-
-
-def pickle_string_to_obj(s):
-    '''
-    从base64反序列化python对象
-    '''
-    return pickle.loads(codecs.decode(s.encode(), "base64"))
+    def pickle_string_to_obj(self,s):
+        '''
+        从base64反序列化python对象
+        '''
+        return pickle.loads(codecs.decode(s.encode(), "base64"))
 
 
 if __name__ == '__main__':
-    # TODO:准备改为嵌入式
-    server = FLServer(GlobalModel_MNIST_CNN, "127.0.0.1", 5000)
-    print("listening on 127.0.0.1:5000")
+    server=FLServer(GlobalModel_MNIST_CNN,'127.0.0.1',5000)
     server.start()
