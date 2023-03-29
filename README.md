aef4c7109afca9c56fb707f91926647c6d696b8b
chang_geng <2154695037@qq.com>
2023-03-29 09:52:44 +0800

    重构gan攻击，单元测试完毕，等待整合

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-## FL通信流程
-![fl通信流程示意图](readmeSrcs\FL通信流程.png)
-
 ## 更新日志
-### <font color="red">2023/3/19 更新</font>
+### <font color="yellow">2023/3/19 更新</font>
 + 重构项目中以'server.py'与'client.py'为主体的联邦学习模块
 + 将联邦学习模块整合入服务器路由，现可通过'/fl_server'与'/fl_client'路由对服务器与客户端进行分别访问
 + 增加'logs'目录用以存放联邦学习产生的日志，联邦学习关闭后可自动删除对应日志 (计划之后各模块日志都附属于此目录下)
@@ -11,3 +8,12 @@
 + <font color="red">无法将联邦学习过程中产生的tensorflow日志重定位至标准日志文件，会导致前端无法清晰地显示训练过程</font>
 + <font color="yellow">用于debug的输出残留</font>
 + <font color="yellow">测试阶段，联邦学习服务器将在连接两台客户机后自动开始分发模型并训练</font>
+
+### <font color="red">2023/3/29 更新</font>
++ 重构gan攻击模块并进行了单元测试: 对mnist数据集表现可行
+  + 涉及文件: gan_attack/gan.py
+  + (被gitignore)附加目录: gan_attack/images 用于保存攻击生成的图片
+  + (被gitignore)附加目录: gan_attack/models 用于保存模型权重 (仅为节省测试时间)
++ gan模块等待整合至联邦学习中
++ <font color="yellow">由于gan攻击测试中发现生成图片有规则乱码区域，经检查后发现是原有cnn架构卷积核大小不合适导致边缘更新被略去，故将联邦学习中所用模型前两部卷积部分改为 5*5 卷积核。经测试，此改变未对项目表现造成影响</font>
+    + 涉及文件 server.py
--- gan_attack/gan.py
+++ gan_attack/gan.py
@@ -1,58 +1,71 @@
 from __future__ import print_function, division
 
+import os
 from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Flatten, LeakyReLU
+from keras.layers import *
 from keras.layers import BatchNormalization
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import numpy as np
 
-import sys
+class LocalModel:
+    '''
+    模拟本地模型,检验gan攻击模块的可插拔性,仅用于测试
+    对应 server.py 与 client.py 的全局模型与本地模型
+    '''
+    def __init__(self):
+        self.model = Sequential()
+        self.model.add(Convolution2D(32, (5, 5), input_shape=(28,28,1)))    
+        # 需要换为5*5卷积核以将28*28全部卷积以防止生成时的由于未更新导致的边角乱码问题
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Convolution2D(32, (5, 5)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Convolution2D(64, (3, 3)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Flatten())
+        self.model.add(Dense(64))
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.3))
+        self.model.add(Dense(10))
+        self.model.add(Activation('softmax'))
 
-savedStdout = sys.stdout 
-print_log = open('log.txt',"w")
-sys.stdout = print_log
+        # self.model.summary()
 
+        # 由于未知原因，优化器不同也会影响攻击的效果
+        self.model.compile(optimizer=Adam(0.0002, 0.5),loss='binary_crossentropy',metrics=['accuracy'])
+        if os.path.exists('gan_attack/models/discriminator.h5'):    # 测试时为训练可重入节约时间
+            self.model.load_weights('gan_attack/models/discriminator.h5')
+    
+    def train(self,sample,t_epochs):
+        '''
+        sample为元组,默认x在[-1,1],y为独热编码
+        t_epochs仅用于模拟服务器每一轮分发
+        '''
+        self.model.trainable=True
+        (X_sample,Y_sample)=sample
+        self.model.fit(X_sample,Y_sample,epochs=t_epochs,batch_size=64,shuffle=True)
+        self.model.trainable=False
 
 class GAN:
-    def __init__(self):
-        self.img_rows = 28
-        self.img_cols = 28
-        self.channels = 1
-        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+    def __init__(self,discriminator:LocalModel):
         self.latent_dim = 100
-
-        optimizer = Adam(0.0002, 0.5)
-
-        # Build and compile the discriminator
-        self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss='binary_crossentropy',
-                                   optimizer=optimizer,
-                                   metrics=['accuracy'])
-
-        # Build the generator
+        self.discriminator=discriminator
         self.generator = self.build_generator()
-
-        # The generator takes noise as input and generates imgs
+        if os.path.exists('gan_attack/models/generator.h5'):
+            self.generator.load_weights('gan_attack/models/generator.h5')
+        
+        # 建立联合模型
         z = Input(shape=(self.latent_dim,))
-        img = self.generator(z)
-
-        # For the combined model we will only train the generator
-        self.discriminator.trainable = False
-
-        # The discriminator takes generated images as input and determines validity
-        validity = self.discriminator(img)
-
-        # The combined model  (stacked generator and discriminator)
-        # Trains the generator to fool the discriminator
-        self.combined = Model(z, validity)
-        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+        validity = self.discriminator.model(self.generator(z))
+        self.combined = Model(inputs=z, outputs=validity)   # 初始化生成器与判别器联合模型
+        self.combined.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5),metrics=['accuracy'])
 
     def build_generator(self):
-
         model = Sequential()
-
         model.add(Dense(256, input_dim=self.latent_dim))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.8))
@@ -62,101 +75,86 @@ class GAN:
         model.add(Dense(1024))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
-        model.add(Reshape(self.img_shape))
+        model.add(Dense(np.prod((28,28,1)), activation='tanh'))
+        model.add(Reshape((28,28,1)))
 
-        model.summary()
+        # model.summary()
 
         noise = Input(shape=(self.latent_dim,))
         img = model(noise)
 
-        return Model(noise, img)
+        return Model(inputs=noise, outputs=img)
+    
+    def trainDiscriminator(self,fakeEpochs,sampleNum,t_epochs):
+        '''
+        本地模型的训练权还是可被攻击者任意调用的
+        t_epochs 测试所用
+        '''
+        # 使用标识为非推断类的生成数据训练本地模型,逼迫暴露更多特征
+        noise=np.random.normal(0, 1, (sampleNum, self.latent_dim))
+        X_fake=self.generator.predict(noise)
+        Y_fake=np.zeros((sampleNum,10))
+        for i in range(sampleNum):
+            Y_fake[i,np.random.randint(low=0,high=9)]=1
+        self.discriminator.train((X_fake,Y_fake),fakeEpochs)
+        
+        # 模拟服务器的合法参与者对模型的纠正训练
+        (X_train, Y_train), (_,_) = mnist.load_data()
+        train_pos=np.random.randint(0,X_train.shape[0],sampleNum)    
+        # 正确训练集和错误训练集的规模差距不能太大才能实现逼迫暴露更多特征的目的
+        X_train=X_train[train_pos]/127.5-1
+        Y_train=tf.one_hot(indices=Y_train[train_pos],depth=10,axis=1)
+        self.discriminator.train((X_train,Y_train),t_epochs)    
+        # 此处epoch仅为了模拟使用，实际上服务器-客户机需要进行t_epochs次的联邦学习
 
-    def build_discriminator(self):
+        # 注意: 此处违反了模块设计准则，仅为了测试时保证时间花销所用
+        self.discriminator.model.save_weights('gan_attack/models/discriminator.h5')
 
-        model = Sequential()
+    def train(self,epochs,trainNum,times):
+        '''
+        训练生成器
+        '''
+        noise = np.random.normal(0, 1, (trainNum, self.latent_dim))
+        valid=np.zeros((trainNum,10))
+        valid[:,-1]=1   # 选定9为要推理内容的类
+        self.combined.fit(noise,valid,epochs=epochs,batch_size=64)
 
-        model.add(Flatten(input_shape=self.img_shape))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(256))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(1, activation='sigmoid'))
-        model.summary()
+        if times%5==0:
+            self.sample_images(f'{times}')
 
-        img = Input(shape=self.img_shape)
-        validity = model(img)
-
-        return Model(img, validity)
-
-    def train(self, epochs, batch_size=128, sample_interval=50):
-
-        # Load the dataset
-        (X_train, _), (_, _) = mnist.load_data()
-
-        # Rescale -1 to 1
-        X_train = X_train / 127.5 - 1.
-        X_train = np.expand_dims(X_train, axis=3)
-
-        # Adversarial ground truths
-        valid = np.ones((batch_size, 1))
-        fake = np.zeros((batch_size, 1))
-
-        for epoch in range(epochs):
-
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
-
-            # Select a random batch of images
-            idx = np.random.randint(0, X_train.shape[0], batch_size)
-            imgs = X_train[idx]
-
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
-
-            # Generate a batch of new images
-            gen_imgs = self.generator.predict(noise)
-
-            # Train the discriminator
-            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
-            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-
-            # ---------------------
-            #  Train Generator
-            # ---------------------
-
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
-
-            # Train the generator (to have the discriminator label samples as valid)
-            g_loss = self.combined.train_on_batch(noise, valid)
-
-            # Plot the progress
-            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
-
-            # If at save interval => save generated image samples
-            if epoch % sample_interval == 0:
-                self.sample_images(epoch)
+        self.generator.save_weights('gan_attack/models/generator.h5')
 
     def sample_images(self, epoch):
+        '''
+        保存该次输出的图片
+        '''
         r, c = 5, 5
         noise = np.random.normal(0, 1, (r * c, self.latent_dim))
         gen_imgs = self.generator.predict(noise)
 
-        # Rescale images 0 - 1
+        # 将[-1,1]的输出规整至[0,1]以便显示
         gen_imgs = 0.5 * gen_imgs + 0.5
 
         fig, axs = plt.subplots(r, c)
-        cnt = 0
         for i in range(r):
             for j in range(c):
-                axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
+                axs[i, j].imshow(gen_imgs[i*5+j, :, :, 0], cmap='gray')
                 axs[i, j].axis('off')
-                cnt += 1
-        fig.savefig("images/%d.png" % epoch)
+        fig.savefig(f"gan_attack/images/{epoch}.png")
         plt.close()
 
 
 if __name__ == '__main__':
-    gan = GAN()
-    gan.train(epochs=30000, batch_size=32, sample_interval=200)
+    dis=LocalModel()
+    gan = GAN(dis)
+
+    # 先对discriminator预训练以模拟已存在的全局模型
+    (X_train,Y_train),(_,_)=mnist.load_data()
+    X_train=X_train/127.5-1
+    Y_train=tf.one_hot(indices=Y_train,depth=10,axis=1)
+    dis.train((X_train,Y_train),5)
+
+    # 模拟gan攻击,共76轮
+    for i in range(76):
+        gan.train(5,512,i)
+        gan.trainDiscriminator(25,1024,5)   # 小的错误训练集生成是为了加快训练速度   
